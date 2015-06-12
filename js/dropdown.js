287d34bdb1d02f8ddbc52a27ae5a4b8922eba631
AnNavara <navarachkou@gmail.com>
2015-06-12 21:09:58 +0500

    add dropdown icon

--- js/dropdown.js
+++ js/dropdown.js
@@ -6,6 +6,7 @@
 	button.addEventListener("tap", function(e) {
 		e.preventDefault();
 		element.classList.toggle("dropdown-menu__container--show");
+		button.classList.toggle("dropdown-menu__toggle--active");
 	});
 
 	// document.addEventListener("tap", function(e) {
@@ -17,6 +18,7 @@
 	window.addEventListener("keydown", function(event) {
 	    if (event.keyCode == 27 && element.classList.contains("dropdown-menu__container--show")) {
 	        element.classList.remove("dropdown-menu__container--show");
+					button.classList.remove("dropdown-menu__toggle--active");
 	  	}
 	});
 
